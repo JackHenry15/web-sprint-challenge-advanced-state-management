1ba2a3e724c4280a00d8096e211650b3f6b2dab5
Jack Henry <jackson.lewis.henry@gmail.com>
2021-03-19 14:02:03 -0400

    wahoo working on actions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
+import { connect } from 'react-redux';
+import {fetchSmurfs} from './actions';
 
 import AddForm from './components/AddForm';
 import SmurfList from './components/SmurfList';
@@ -8,6 +10,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 
 class App extends Component {
+
+  
   render() {
     return (
       <div className="App">
@@ -22,7 +26,14 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return{
+    isLoading: state.isLoading,
+    smurfs: state.smurfs,
+    error: state.error
+  }
+}
+export default connect(mapStateToProps, { fetchSmurfs })(App);
 
 //Task List:
 //1. Connect the fetchSmurfs actions to the App component.
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,28 @@
 import axios from 'axios';
 
+//async action
+export const FETCH_SMURF_SUCCESS = "FETCH_SMURF_SUCCESS";
+export const FETCH_SMURF_START = "FETCH_SMURF_START";
+export const FETCH_SMURF_FAILURE = "FETCH_SMURF_FAILURE";
+export const fetchSmurfs = () => {
+    return (dispatch) => {
+        //loading
+        dispatch({ type: FETCH_SMURF_START });
+        //starting fetch
+        axios
+            .get(`http://localhost:3333/smurfs`)
+            .then((res) => {
+                console.log(res.data);
+                //reducer gets sent data
+                dispatch({ type: FETCH_SMURF_SUCCESS, payload: res.data });
+            })
+            .catch((err) => {
+                console.log(err.message);
+                dispatch({ type: FETCH_SMURF_FAILURE, payload: err.message });
+            });
+    };
+};
+
 //Task List:
 //1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
 //2. Add a standard action that allows us to add new smurf (including the name, nickname, position, summary)
